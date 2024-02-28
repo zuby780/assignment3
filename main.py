a7d44e0992acf3cc7c926fb83ccec4b1aa1c0c3b
zuby780 <161565893+zuby780@users.noreply.github.com>
2024-02-28 15:19:17 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+name = "zubaiar"
+age = 20
+address = "Cisaat"
+hobby = "Cricket"
+gender = "Male"
+
+print("My name is", name)
+print("My age ",age)
+print("I live in ",address)
+print("My hobbies are ",hobby)
+print("Gender ",gender)
